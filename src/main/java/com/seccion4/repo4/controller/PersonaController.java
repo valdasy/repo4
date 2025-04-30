2f866cf4d131bf6ced8ca9b4c700fd8deff1fd9e
alejandro <alejandrobricenobenavides@gmail.com>
2025-04-30 14:38:05 -0400

    no puedo mas

--- src/main/java/com/seccion4/repo4/controller/PersonaController.java
+++ src/main/java/com/seccion4/repo4/controller/PersonaController.java
@@ -2,18 +2,36 @@ package com.seccion4.repo4.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seccion4.repo4.model.Persona;
+import com.seccion4.repo4.service.PersonaService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
 @RequestMapping("/api/personas")
-@RestController
+@RestController("")
 public class PersonaController {
+    @Autowired
+    private PersonaService personaService;
     
-    @GetMapping
-    public String Saludar()
-    {
-        return "Hola esto esta funcionando !!";
+    @PostMapping 
+    public Persona postPersona(@RequestBody Persona persona){
+        return personaService.guardarPersona(persona);
+    }
+
+    @GetMapping()
+    public List<Persona> getPersonas() {
+        return personaService.listarPersonas() ;
     }
     
 }
--- src/main/java/com/seccion4/repo4/repository/PersonaRepository.java
+++ src/main/java/com/seccion4/repo4/repository/PersonaRepository.java
@@ -3,13 +3,21 @@ package com.seccion4.repo4.repository;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.seccion4.repo4.model.Persona;
+import org.springframework.stereotype.Repository;
 
+import com.seccion4.repo4.model.Persona;
+@Repository
 public class PersonaRepository {
     private List<Persona> personas=new ArrayList<>();
 
     public Persona create(Persona persona){
         personas.add(persona);
+        System.out.println(persona.toString());
         return persona;
     }
+    
+    public List<Persona> readAll(){
+        return personas;
+    }
+    
 }
