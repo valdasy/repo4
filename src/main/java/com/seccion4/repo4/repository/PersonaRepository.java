a76ffeaf76a04a247ec4dea761b4956146b70e05
alejandro <alejandrobricenobenavides@gmail.com>
2025-04-30 14:50:27 -0400

    ahora continuo

--- src/main/java/com/seccion4/repo4/repository/PersonaRepository.java
+++ src/main/java/com/seccion4/repo4/repository/PersonaRepository.java
@@ -9,7 +9,15 @@ import com.seccion4.repo4.model.Persona;
 @Repository
 public class PersonaRepository {
     private List<Persona> personas=new ArrayList<>();
+    PersonaRepository(){
+        personas.add(new Persona(1,  "12345678-9",  "wacoldo", "Soto", 45));
+        personas.add(new Persona(2, "98765432-1", "María", "Pérez", 32));
+        personas.add(new Persona(3, "11222333-4", "Juan", "González", 28));
+        personas.add(new Persona(4, "55667788-5", "Lucía", "Ramírez", 37));
+        personas.add(new Persona(5, "99887766-2", "Carlos", "Muñoz", 50));
+        personas.add(new Persona(6, "33445566-7", "Ana", "Fernández", 41));
 
+    }
     public Persona create(Persona persona){
         personas.add(persona);
         System.out.println(persona.toString());
